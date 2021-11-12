7ae3c8dd3c7eac96a1cf5bf736fe094609e8c9dc
Viraj Jalindar Raut <90141051+virajraut48@users.noreply.github.com>
2021-11-12 21:16:37 +0530

    Add files via upload

---
+++ ConnectivityAssignment.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectivity.assignment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author saqib khan
+ */
+public class ConnectivityAssignment {
+    Connection conn;
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3307/conn","root","");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+            
+            
+        }
+        
+        
+        
+    }
+    
+}
